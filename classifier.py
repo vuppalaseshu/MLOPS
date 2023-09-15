d6eb7ef72612b623563c40ac2efd9ffa35a08698
vuppalaseshu <vuppala.seshu@gmail.com>
2023-09-15 10:56:34 +0000

    second commit

--- classifier.py
+++ classifier.py
@@ -2,13 +2,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier  
 
 df = pd.read_csv("/content/MLOPS/Data/Iris.csv")
 features = ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
 target = 'Species'
 
 x_train,x_test,y_train,y_test = train_test_split(df[features],df[target],test_size=0.3, shuffle=True)
-clf=DecisionTreeClassifier(criterion="entropy")
+clf=RandomForestClassifier(n_estimators=10, criterion="entropy")
 
 clf.fit(x_train,y_train)
 
